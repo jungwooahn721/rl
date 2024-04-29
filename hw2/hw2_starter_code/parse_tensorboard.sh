f3c51617b3d6c0f882af4975413069e867dacb14
jungwooahn721 <jungwooahn721@yonsei.ac.kr>
2024-04-29 18:41:09 +0000

    add log data

--- hw2/hw2_starter_code/parse_tensorboard.sh
+++ hw2/hw2_starter_code/parse_tensorboard.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-log_folder_1="q2_pg_cartpole_lb_CartPole-v0_29-04-2024_15-41-08"
-log_folder_2="q2_pg_cartpole_lb_rtg_CartPole-v0_29-04-2024_15-41-43"
-log_folder_3="q2_pg_cartpole_lb_na_CartPole-v0_29-04-2024_15-42-13"
-log_folder_4="q2_pg_cartpole_lb_rtg_na_CartPole-v0_29-04-2024_15-42-39"
+log_folder_1="q2_pg_cheetah_HalfCheetah-v4_29-04-2024_16-01-36"
+log_folder_2="q2_pg_lunar_lander_lambda0.95_LunarLander-v2_29-04-2024_16-38-24"
+log_folder_3="q2_pg_lunar_lander_lambda0.98_LunarLander-v2_29-04-2024_17-11-55"
+log_folder_4="q2_pg_lunar_lander_lambda0.99_LunarLander-v2_29-04-2024_17-40-10"
 
-title="Learning Curves (large batch experiments)"
+title="w/, w/o Baseline Comparison on HalfCheetah-v4"
 
 python aai4160/scripts/parse_tensorboard.py \
     --input_log_files data/$log_folder_1 data/$log_folder_2 data/$log_folder_3 data/$log_folder_4 \
-    --human_readable_names "Vanilla" "RewToGo" "NormAdv" "RewToGo+NormAdv" \
+    --human_readable_names "Vanilla" "Baseline" \
     --data_key "Eval_AverageReturn" \
-    --title "Learning Curves (small batch experiments)" \
+    --title "$title" \
     --x_label_name "Train Environment Steps" \
     --y_label_name "Eval Return" \
     --output_file "$title.png" \
