7eab2c4c81e81fdbed7f324c2e27cab88530d9af
jungwooahn721 <jungwooahn721@yonsei.ac.kr>
2024-04-30 13:16:53 +0000

    add pdf

--- hw2/hw2_starter_code/parse_tensorboard_baseline.sh
+++ hw2/hw2_starter_code/parse_tensorboard_baseline.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 log_folder_1="q2_pg_cheetah_HalfCheetah-v4_30-04-2024_10-52-33"
 log_folder_2="q2_pg_cheetah_baseline_HalfCheetah-v4_30-04-2024_10-52-56"
-log_folder_3="q2_pg_cheetah_baseline_blr0.005_HalfCheetah-v4_30-04-2024_10-58-20"
-log_folder_4="q2_pg_cheetah_baseline_bgs2_HalfCheetah-v4_30-04-2024_10-58-31"
+#log_folder_3="q2_pg_cheetah_baseline_blr0.005_HalfCheetah-v4_30-04-2024_10-58-20"
+#log_folder_4="q2_pg_cheetah_baseline_bgs2_HalfCheetah-v4_30-04-2024_10-58-31"
 
-title="with, without baseline + lower learning rate, gradient step"
+title="with, without baseline"
 
 python aai4160/scripts/parse_tensorboard.py \
-    --input_log_files data/$log_folder_1 data/$log_folder_2 data/$log_folder_3 data/$log_folder_4 \
-    --human_readable_names "w/o baseline" "w/ baseline" "w/ baseline, blr: 0.005" " w/ baseline, bgs: 2" \
+    --input_log_files data/$log_folder_1 data/$log_folder_2 \
+    --human_readable_names "w/o baseline" "w/ baseline" \
     --data_key "Eval_AverageReturn" \
     --title "$title" \
     --x_label_name "Train Environment Steps" \
